0f04a2b4e53375788d5b68d409f040914327d2b2
Rodrigo Gamboa <rodrigogamboatorres@gmail.com>
2020-11-04 14:54:15 -0600

    Created chronometer challenge. chronometer.js completed

---
+++ lab-canvas-race-car/js/index.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+  document.getElementById('start-button').onclick = () => {
+    startGame();
+  };
+
+  function startGame() {
+    // var canvas = document.getElementById('canvas');
+    // var ctx = canvas.getContext('2d');
+    // var img = new Image();
+    // img.addEventListener('load', function(){
+
+    // }, false);
+    // img.src = 'images/road.png';
+  }
+};
---
+++ lab-javascript-chronometer/javascript/chronometer.js
@@ -0,0 +1,44 @@
+class Chronometer {
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
+
+  startClick(){
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+    }, 1000);
+  }
+
+  getMinutes() {
+    let minutes = Math.floor(this.currentTime/60);
+    return minutes;
+  }
+
+  getSeconds() {
+    let seconds = this.currentTime - this.getMinutes() * 60;
+    return seconds;
+  }
+
+  twoDigitsNumber(num) {
+    if(num < 9) {
+      return `0${num}`;
+    } else {
+      return `${num}`;
+    }
+  }
+
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
+
+  resetClick() {
+    this.currentTime = 0;
+  }
+
+  splitClick() {
+    const minutes = this.twoDigitsNumber(this.getMinutes());
+    const seconds = this.twoDigitsNumber(this.getSeconds());
+    return `${minutes}:${seconds}`;
+  }
+}
